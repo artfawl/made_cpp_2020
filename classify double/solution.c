3bab05e24bb996116d58d5994e48ac31a47b15c1
artfawl <stark050600@mail.ru>
2020-09-19 23:25:59 +0300

    hw1

--- classify double/solution.c
+++ classify double/solution.c
@@ -14,8 +14,8 @@ uint64_t convertToUint64 (double number) {
     return *((uint64_t *)(&number));
 }
 
-bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+bool getBit(const uint64_t number, const uint8_t index) {
+	return (bool)((((uint64_t)1 << index) & number) >> index);
 }
 
 
@@ -23,44 +23,82 @@ bool getBit (const uint64_t number, const uint8_t index) {
  * Checkers here:
  */
 
-bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+bool checkForPlusZero(uint64_t number) {
+	return number == 0;
 }
 
 bool checkForMinusZero (uint64_t number) {
     return number == 0x8000000000000000;
 }
 
-bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+bool checkForPlusInf(uint64_t number) {
+	bool temp = 1;
+	for (int i = 52; i <= 62; i++) {
+		temp = temp && getBit(number, i);
+	}
+	for (int i = 0; i <= 51; i++) {
+		temp = temp && !getBit(number, i);
+	}
+	return temp && !getBit(number, 63);
 }
 
-bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+bool checkForMinusInf(uint64_t number) {
+	return checkForPlusInf(number << 1 >> 1) && getBit(number, 63);
 }
 
-bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+bool checkForPlusNormal(uint64_t number) {
+	int temp = 0;
+	for (int i = 52; i <= 62; i++)
+		temp += getBit(number, i);
+	if (temp < 11 && temp >= 1)
+		return 1 && !getBit(number, 63);
+	return 0;
 }
 
-bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusNormal(uint64_t number) {
+	return checkForPlusNormal(number << 1 >> 1) && getBit(number, 63);
 }
 
-bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+bool checkForPlusDenormal(uint64_t number) {
+	bool temp_for_fract = 0;
+	bool temp_for_exp = 1;
+	for (int i = 0; i <= 51; i++) {
+		temp_for_fract = temp_for_fract || getBit(number, i);
+	}
+	for (int i = 52; i <= 62; i++) {
+		temp_for_exp = temp_for_exp && !getBit(number, i);
+	}
+	if (temp_for_fract && temp_for_exp)
+		return 1 && !getBit(number, 63);
+	return 0;
 }
 
-bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusDenormal(uint64_t number) {
+	return checkForPlusDenormal(number << 1 >> 1) && getBit(number, 63);
 }
 
-bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+bool checkForSignalingNan(uint64_t number) {
+	bool temp_for_exp = 1;
+	bool temp_for_fract = 0;
+	for (int i = 0; i <= 50; i++) {
+		temp_for_fract = temp_for_fract || getBit(number, i);
+	}
+	for (int i = 52; i <= 62; i++) {
+		temp_for_exp = temp_for_exp && getBit(number, i);
+	}
+	if (temp_for_fract && !getBit(number, 51) && temp_for_exp)
+		return 1;
+	return 0;
 }
 
-bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+bool checkForQuietNan(uint64_t number) {
+	bool temp_for_exp = 1;
+	for (int i = 52; i <= 62; i++) {
+		temp_for_exp = temp_for_exp && getBit(number, i);
+	}
+	if (temp_for_exp && getBit(number, 51))
+		return 1;
+	return 0;
 }
 
 
