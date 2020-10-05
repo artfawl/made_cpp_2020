26f045cda244fbbddb525cb67bb4e60bb07e2021
artfawl <stark050600@mail.ru>
2020-10-05 12:03:37 +0300

    simple_compose

--- simple_compose/src/main.cpp
+++ simple_compose/src/main.cpp
@@ -1,32 +1,56 @@
 #include <iostream>
 #include <functional>
 
-typedef std::function<int (int)> Op;
+typedef std::function<int(int)> Op;
 
 
 
-Op compose (size_t n, Op ops[]) {
+Op compose(size_t n, Op ops[]) {
     /// Your code goes here.
+    if (n == 0) {
+        auto foo = [](int arg) {
+            return arg;
+        };
+        return foo;
+    }
+    else {
+        auto foo = [&n, &ops](int arg) {
+            int res = ops[n - 1](arg);
+            res = compose(n - 1, ops)(res);
+            return res;
+        };
+        return foo;
+    }
+}
+
+Op compose1(size_t n, Op ops[]) {
+    //None reqursive function
+    auto foo = [&n, &ops](int arg) {
+        for (ssize_t i = n - 1; i >= 0; --i)
+            arg = ops[i](arg);
+        return arg;
+    };
+    return foo;
 }
 
 
-int main () {
+int main() {
     /// Simple tests:
 
     Op op1 =
-        [] (int x) -> int {
-            return x + 1;
-        };
+        [](int x) -> int {
+        return x + 1;
+    };
 
     auto op2 =
-        [] (int p) -> Op {
-            return [p] (int x) {
-                return p * x;
-            };
+        [](int p) -> Op {
+        return [p](int x) {
+            return p * x;
         };
+    };
 
     {
-        Op ops[4] = {op1, op2(2), op1, op2(2)};
+        Op ops[4] = { op1, op2(2), op1, op2(2) };
         if (compose(4, ops)(2) != 11) {
             std::cout << "FAILED AT TEST 1" << std::endl;
             return 0;
@@ -34,7 +58,7 @@ int main () {
     }
 
     {
-        Op ops[8] = {op1, op2(2), op1, op1, op1, op2(2), op2(2), op1};
+        Op ops[8] = { op1, op2(2), op1, op1, op1, op2(2), op2(2), op1 };
         if (compose(8, ops)(5) != 55) {
             std::cout << "FAILED AT TEST 2" << std::endl;
             return 0;
@@ -42,7 +66,7 @@ int main () {
     }
 
     {
-        Op ops[1] = {op1};
+        Op ops[1] = { op1 };
         if (compose(1, ops)(3) != 4) {
             std::cout << "FAILED AT TEST 3" << std::endl;
             return 0;
@@ -58,7 +82,7 @@ int main () {
     }
 
     {
-        Op ops[4] = {op2(2), op2(3), op2(4), op2(5)};
+        Op ops[4] = { op2(2), op2(3), op2(4), op2(5) };
         if (compose(4, ops)(1) != 120) {
             std::cout << "FAILED AT TEST 5" << std::endl;
             return 0;
